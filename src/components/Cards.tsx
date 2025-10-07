b4d86959b072af1cd8a10be78563cf34b4f2ef42
simonecontidev <barriotropical@gmail.com>
2025-10-07 10:09:00 +0200

    card component, global css style
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

---
+++ src/components/Cards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+const Card = () => {
+  return (
+    <Box className="cardBackground" sx={{padding: "2rem", borderRadius: "0.75rem"}}>
+      Card
+    </Box>
+  )
+}
+
+export default Card;
