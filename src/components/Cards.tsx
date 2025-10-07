3710b297c4ffca9b298e4a3a35d1fede279f4fae
simonecontidev <barriotropical@gmail.com>
2025-10-07 10:28:33 +0200

    Card info structure
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 
 const Card = () => {
   return (
-    <Box className="cardBackground" sx={{padding: "2rem", borderRadius: "0.75rem"}}>
-      Card
+    <Box
+      className="cardBackground"
+      sx={{
+        padding: "2rem",
+        borderRadius: "0.75rem",
+        textAlign: "center",
+      }}
+    >
+      <Image
+        src="/assets/images/portrait.jpg"
+        alt="Portrait"
+        width={88}
+        height={88}
+        style={{ borderRadius: "50%" }}
+      />
+      <Typography variant="h1" sx={{ fontSize:"2.25rem", fontWeight: 700, color: "#FFFFFF", marginTop: "1rem" }}>
+        Simone Conti
+      </Typography>
+      <Typography variant="h2" className="neon" sx={{ fontSize: "1.3125rem", marginTop: "0.5rem"}}>
+        Barcelona, Spain
+      </Typography>
+      <Typography sx={{ fontSize: "1rem", marginTop: "0.5rem"}}>Front-end Developer, blend code with creative interaction
+      </Typography>
     </Box>
-  )
-}
+  );
+};
 
 export default Card;
